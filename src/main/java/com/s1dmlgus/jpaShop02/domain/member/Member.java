2f1348dac1c64a3a746a19c8f4330329eb2534d2
t1dmlgus <dmlgusgngl@gmail.com>
2021-09-13 17:04:25 +0900

    feat: 회원 엔티티 구현

--- src/main/java/com/s1dmlgus/jpaShop02/domain/member/Member.java
+++ src/main/java/com/s1dmlgus/jpaShop02/domain/member/Member.java
@@ -1,4 +1,18 @@
 package com.s1dmlgus.jpaShop02.domain.member;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
 public class Member {
+
+    private Long id;
+    private String name;
 }
