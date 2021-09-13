4d02a13f4d1ba580615cb7f31160c1858b115ac5
t1dmlgus <dmlgusgngl@gmail.com>
2021-09-13 20:32:56 +0900

    feat: 상품 서비스 구현
    - 상품 등록
    - 상품 수정(merge)
    - 상품 목록 조회

---
+++ src/main/java/com/s1dmlgus/jpaShop02/domain/Item/ItemRepository.java
@@ -0,0 +1,29 @@
+package com.s1dmlgus.jpaShop02.domain.Item;
+
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class ItemRepository {
+
+    @PersistenceContext
+    EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) {
+            em.persist(item);
+        }else {
+            em.merge(item);
+        }
+
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("SELECT i FROM Item i", Item.class)
+                .getResultList();
+    }
+}
